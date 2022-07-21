5f65d9be8c21cf77820f8b4f6f43d27f4659d23a
GuessingMachine <tjd125gns@naver.com>
2022-07-22 00:58:01 +0900

    -
    @AutoWired  : DI하는 Xml의 설정을 대신하는 어노테이션
    IoC 컨테이너 :  객체를 생성하고 그들을 조립하는 과정에서 객체를 담는 그릇
    
    @AutoWired의 작동원리 : 자료형을 찾아서 구분, 자료형으로 안되면 변수명으로도 식별
    
    @Qualifier 로 변수이를을 적어서 @AutoWired를 지정
    
    - @AutoWired의 위치와 Required 옵션
    
    Injection 방법 3가지 (@AutoWired의 위치)
    1. 필드에 적기
    2. 오버로드 생성자위에 적기
    3. 세터 함수 위에 적기
    
    @AutoWired (required = false) // 지시서에 객체 생성이 없어도 진행가능

--- src/spring/di/ui/InlineExamConsole.java
+++ src/spring/di/ui/InlineExamConsole.java
@@ -5,12 +5,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import spring.di.entity.Exam;
 
 public class InlineExamConsole implements ExamConsole {
-
+	// 1번 위치 @Autowired
 	private Exam exam;
 	
 	public InlineExamConsole() {
 	}
 	
+	// 2번 위치 @Autowired
+	// @Qualifier("exam2")매개변수에 적음
 	public InlineExamConsole(Exam exam) {
 		this.exam = exam;
 	}
@@ -21,7 +23,7 @@ public class InlineExamConsole implements ExamConsole {
 		System.out.printf("total is %d, avg is %f\n", exam.total(), exam.avg());
 	}
 
-	@Autowired //xml 설정파일에 <property name="exam" ref = "exam"></property> 부분을 대신하게 됨 
+	// 3번 위치 @Autowired //xml 설정파일에 <property name="exam" ref = "exam"></property> 부분을 대신하게 됨 
 	@Override
 	public void setExam(Exam exam) {
 		this.exam = exam;
